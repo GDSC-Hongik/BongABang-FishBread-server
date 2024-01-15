bf87cd7535fe12d3e7dbec360eb0bc7bb2ca65fd
SeungHoon <pokjm99@gmail.com>
2024-01-16 02:15:25 +0900

    Mysql settings

---
+++ Bong_A_Bang/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Bong_A_Bang project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bong_A_Bang.settings')
+
+application = get_asgi_application()
