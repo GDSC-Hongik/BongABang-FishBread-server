314c88638324dc91c7d4bd7f1f4e58be6b9d2142
SeungHoon <pokjm99@gmail.com>
2024-01-20 21:38:20 +0900

    Delete first, last name, Add image field

--- cafe/models.py
+++ cafe/models.py
@@ -4,6 +4,8 @@ from .managers import CafeOwnerManager
 
 class CafeOwner(AbstractUser, PermissionsMixin):
     username = None
+    first_name = None
+    last_name = None
     # 유저 정보
     email = models.EmailField(unique=True)
     owner_phone_number = models.CharField(max_length=15, blank=True, null=True)
@@ -13,6 +15,7 @@ class CafeOwner(AbstractUser, PermissionsMixin):
     cafe_name = models.CharField(max_length=100)
     cafe_address = models.CharField(max_length=255)
     cafe_phone_number = models.CharField(max_length=15)
+    cafe_image = models.ImageField(upload_to='menu_images', default='default_image.jpg')
     
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
