c313fa5d92e2a132543027b3466069400e40e52e
SeungHoon <pokjm99@gmail.com>
2024-01-20 23:14:03 +0900

    Feat : Change login url name

--- cafe/urls.py
+++ cafe/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import CafeOwnerRegisterView,CustomLoginView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', CafeOwnerRegisterView.as_view(), name='register'),
 ]
