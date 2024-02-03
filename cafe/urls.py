ea3ed547277d30506551e0b9975b8cb5454c1917
Jeon-jisu <wltnjeon0119@gmail.com>
2024-02-03 13:30:09 +0900

    feat: implement tts feature

--- cafe/urls.py
+++ cafe/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
-from .views import CafeOwnerRegisterView,CustomLoginView,register,login,home,query_view,query_view2,cafe_menu,some_menu
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import CafeOwnerRegisterView,CustomLoginView,register,login,home,query_view,cafe_menu,some_menu,get_audio_file,get_latest_audio
 urlpatterns = [
     # path('login/', CustomLoginView.as_view(), name='register'),
     # path('register/', CafeOwnerRegisterView.as_view(), name='register'),
@@ -7,6 +9,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('cafe_menu/', cafe_menu, name='cafe_menu'),
     path('some_menu/', some_menu, name='some_menu'),
-    path('chatgpt/',query_view2, name='query_view'),
+    path('chatgpt/',query_view, name='query_view'),
     path('', home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cafe/views.py
+++ cafe/views.py
@@ -5,14 +5,24 @@ from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import ValidationError
 from google.cloud import texttospeech
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse,HttpResponseNotFound
 from django.contrib.auth import login as django_login, authenticate
 from django.shortcuts import render,redirect
 import openai
+from datetime import datetime
+
 from django.conf import settings
-import os
+import os, re
 from .models import Menu
 from .serializers import CafeOwnerRegisterSerializer,CustomLoginSerializer
+
+def get_audio_file(request, file_name):
+    file_path = os.path.join(settings.MEDIA_ROOT, file_name)
+
+    if os.path.exists(file_path):
+        return FileResponse(open(file_path, 'rb'))
+    else:
+        return HttpResponseNotFound("The requested audio file was not found.")
 
 class CafeOwnerRegisterView(APIView):
     permission_classes = [AllowAny]
@@ -76,33 +86,22 @@ def some_menu(request):
     menus = Menu.objects.filter(price_ice__gte=3000)
     return render(request, 'cafe_menu.html', {'cafe_menu': menus})
 
-# chatgpt 연동
-def get_completion(prompt): 
-	print(prompt) 
-	query = openai.ChatCompletion.create( 
-		model="gpt-3.5-turbo",
-		messages=[
-        	{'role':'user','content': prompt}
-    	], 
-		max_tokens=1024, 
-		n=1, 
-		stop=None, 
-		temperature=0.5, 
-	) 
-	response = query.choices[0].message["content"]
-	print(response) 
-	return response 
+def get_latest_audio(request):
+    static_dir = 'static/'  # 정적 파일 폴더 경로
+    audio_files = [f for f in os.listdir(static_dir) if f.endswith('.mp3')]
 
+    # 파일명에서 숫자 추출 및 정렬
+    audio_files.sort(key=lambda f: int(re.search("v(\d+)", f).group(1)), reverse=True)
 
-def query_view(request): 
-	if request.method == 'POST': 
-		prompt = request.POST.get('prompt') 
-		prompt=str(prompt)
-		response = get_completion(prompt)
-		return JsonResponse({'response': response}) 
-	return render(request, 'query.html') 
+    if audio_files:
+        latest_file = audio_files[0]  # 가장 큰 숫자를 가진 파일
+        print(latest_file)
+        return JsonResponse({'latest_audio': latest_file})
+    else:
+        print("no latest_file")
+        return JsonResponse({'error': 'No audio files found'})
 
-def get_completion2(request, user_input): 
+def get_completion(request, user_input): 
     # 대화 이력 확인 및 업데이트
     if 'history' not in request.session:
         request.session['history'] = []
@@ -110,7 +109,7 @@ def get_completion2(request, user_input):
     # 이전 대화 내역을 포함시킴
     previous_conversation = "\n".join([f"{exchange['user']}\\n{exchange['bot']}" for exchange in request.session['history']])
     full_prompt = previous_conversation + "\n\n" + user_input
-
+    print("full_prompt:",full_prompt)
     # OpenAI GPT 모델을 사용하여 대답 생성
     query = openai.ChatCompletion.create( 
         model="gpt-3.5-turbo",
@@ -120,16 +119,16 @@ def get_completion2(request, user_input):
         max_tokens=1024, 
         n=1, 
         stop=None, 
-        temperature=0.5, 
+        temperature=0.1, 
     ) 
     response = query.choices[0].message["content"]
 
     # 새 대화 내역 추가
     request.session['history'].append({'user': user_input, 'bot': response})
-    
+    print(response)
     return response
 
-def run_text_to_speech(text, post_count):
+def run_text_to_speech(text, post_count,now):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/Users/jeonjisu/Desktop/university/project/BongABang-FishBread-server/codebook/bong-a-bang-412508-9e4d0ff505ce.json"
     client = texttospeech.TextToSpeechClient()
     synthesis_input = texttospeech.SynthesisInput(text=text)
@@ -141,7 +140,7 @@ def run_text_to_speech(text, post_count):
     )
     
     # 새로운 파일 이름 생성
-    output_file_name = f"newoutput_v{post_count}.mp3"
+    output_file_name = f"newoutput_v{post_count}{now}.mp3"
     output_file_path = os.path.join(settings.MEDIA_ROOT, output_file_name)
     print("output_file_path",output_file_path)
     response = client.synthesize_speech(
@@ -155,24 +154,37 @@ def run_text_to_speech(text, post_count):
 
     # 새로운 파일 이름과 경로를 반환
     return output_file_name, output_file_path
-def query_view2(request): 
-    if request.method == 'POST': 
-        user_input = request.POST.get('prompt') 
+def query_view(request):
+    print(request.method)
+    if request.method == 'POST':
+        user_input = request.POST.get('prompt')
         user_input = str(user_input)
-        response = get_completion2(request, user_input)
-        
-        # 새로운 파일 이름을 얻어옴
-        post_count = request.session.get('post_count', 0)
-        post_count += 1
-        request.session['post_count'] = post_count
-        new_audio_file_name, new_audio_file_path = run_text_to_speech(response, post_count)
-        print("settings.MEDIA_URL",settings.MEDIA_URL)
-        audio_url = request.build_absolute_uri(settings.MEDIA_URL + new_audio_file_name)
+        print("user_input",user_input)
+        # 사용자 입력이 유효한 경우에만 음성 파일 생성 로직 실행
+        if user_input:
+            response = get_completion(request, user_input)
+            now = str(datetime.now())
 
-        return JsonResponse({'response': response, 'audio_url': audio_url, 'history': request.session['history']})
+            # 새로운 파일 이름을 얻어옴
+            post_count = request.session.get('post_count', 0)
+            post_count += 1
+            request.session['post_count'] = post_count
+            new_audio_file_name, new_audio_file_path = run_text_to_speech(response, post_count, now)
+            
+            # 현재 MP3 파일 경로를 세션에 저장
+            request.session['audio_file_path'] = new_audio_file_path
+            
+            audio_url = '/static/' + new_audio_file_name
+            print("audio_url:", audio_url)
+
+            return JsonResponse({'response': response, 'audio_url': audio_url, 'history': request.session['history']})
+        else:
+            # 사용자 입력이 없는 경우, 경고 메시지와 함께 응답 반환
+            return JsonResponse({'error': 'No user input provided'})
     elif request.method == 'GET':
         # GET 요청 시 대화 세션 초기화
         request.session['started'] = False
         request.session['history'] = []
         request.session['post_count'] = 0
+        request.session['audio_file_path'] = None
     return render(request, 'query.html', {'history': request.session.get('history', [])})
